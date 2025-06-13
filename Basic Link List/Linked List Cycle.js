e618f83daf17b7e97397dfd3145e37b5a103c56c
Jay Solanki <jaysol8900@gmail.com>
2025-06-13 13:32:44 +0530

    moved

---
+++ Basic Link List/Linked List Cycle.js
@@ -0,0 +1,16 @@
+//linked list cycle leetcode 141
+
+
+function hasCycle(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+    return false;
+}
+
